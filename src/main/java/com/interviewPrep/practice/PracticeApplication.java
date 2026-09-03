591e186b6af09100cf106d210496908bf82764c8
KebronDaniel <kebronjrdaniel@gmail.com>
2026-09-03 14:27:59 +0200

    test the rotating array inside main.

--- src/main/java/com/interviewPrep/practice/PracticeApplication.java
+++ src/main/java/com/interviewPrep/practice/PracticeApplication.java
@@ -1,6 +1,8 @@
 package com.interviewPrep.practice;
 
 import com.interviewPrep.practice.coreJava.equalsHashcode.User;
+import com.interviewPrep.practice.dsa.arraysAndStrings.KadanesAlgorithm;
+import com.interviewPrep.practice.dsa.arraysAndStrings.RotateAnArray;
 import com.interviewPrep.practice.dsa.arraysAndStrings.ThreeSums;
 import com.interviewPrep.practice.dsa.arraysAndStrings.TwoSum;
 import org.springframework.boot.SpringApplication;
@@ -15,9 +17,9 @@ public class PracticeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 
-		ThreeSums threeSums = new ThreeSums();
-		var result = threeSums.getThreeSums(new int[]{-4,-1,-1,0,0,1,1,2});
-		System.out.println(result);
+		RotateAnArray rotateAnArray = new RotateAnArray();
+		var result = rotateAnArray.getRotatedArray(new int[]{1,2,3,4,5,6,7},3);
+		System.out.println(Arrays.toString(result));
 	}
 
 }
