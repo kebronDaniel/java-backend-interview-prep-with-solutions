47c930e52d95a8b0dc419015c602439ab426ae9a
KebronDaniel <kebronjrdaniel@gmail.com>
2026-09-15 17:07:56 +0200

    Stack tree

---
+++ src/main/java/com/interviewPrep/practice/dsa/treesAndGraphs/StackTree.java
@@ -0,0 +1,30 @@
+package com.interviewPrep.practice.dsa.treesAndGraphs;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+// stack that stores a tree
+public class StackTree {
+
+    private Deque<TreeNode> stackTreeNode = new ArrayDeque<>();
+    private int size;
+
+    public void add(TreeNode treeNode){
+        stackTreeNode.push(treeNode);
+        size++;
+    }
+
+    public TreeNode pop(){
+        if (isEmpty()) throw new IllegalStateException("Can't pop an empty stack");
+        size--;
+        return stackTreeNode.pop();
+    }
+
+
+
+    public boolean isEmpty(){
+        return size <= 0;
+    }
+}
